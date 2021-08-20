320fc09238aa8f5b32cd2a2e8b7e862551be3b88
@NodirMN <998946140194@mail.ru>
2021-08-19 12:52:51 -1200

    LocolSorage

---
+++ locol.js
@@ -0,0 +1,8 @@
+let ism = document.getElementById('name')
+let yosh = document.getElementById('yosh')
+let sana = document.getElementById('sana')
+let otdel = document.getElementById('otdel')
+
+function add(){
+    console.log(ism.value, yosh.value, sana.value, otdel.value);
+}
